3b4b62ccc63d777a904ca01cd20d0b635ce9b73e
Matt Ziegler <mattziegler@mattzielersimac.att.net>
2014-02-22 16:10:31 -0800

    added a test class

---
+++ MZTest.h
@@ -0,0 +1,13 @@
+//
+//  MZTest.h
+//  Git Test
+//
+//  Created by Matt Ziegler on 2/22/14.
+//  Copyright (c) 2014 Matt Ziegler. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MZTest : NSObject
+
+@end
---
+++ MZTest.m
@@ -0,0 +1,13 @@
+//
+//  MZTest.m
+//  Git Test
+//
+//  Created by Matt Ziegler on 2/22/14.
+//  Copyright (c) 2014 Matt Ziegler. All rights reserved.
+//
+
+#import "MZTest.h"
+
+@implementation MZTest
+
+@end
